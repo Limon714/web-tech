d23f172793f8cb0e33362a9e395d84d0d42590ef
Limon <limon4u714@gmail.com>
2023-07-03 20:05:27 +0600

    Email Added

--- src/app/components/SigninButton.tsx
+++ src/app/components/SigninButton.tsx
@@ -6,8 +6,9 @@ const SigninButton = () => {
     const {data:session} = useSession();
     if(session && session.user){
         return(
-            <div className='flex gap-4 ml-auto'>
+            <div className='flex gap-3 ml-auto grid grid-flow-rows grid-cols-3'>
                 <p className='text-sky-800'>{session.user.name}</p>
+                <span className='w-4 h-4 rounded-full mr-36 '> {session.user.email} </span>
                 <button onClick={() => signOut()} className='text-red-700'>Sign out</button>
             </div>
         )
